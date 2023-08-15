daf3fbcf9bfe55a4be0ebc68b895081a046dffdf
mlr2905 <7585474@gmail.com>
2023-08-15 11:46:43 +0300

    up

---
+++ Class works/03.07.2023/README.md
@@ -0,0 +1,1 @@
+# Welcome to my lesson of HTML CSS
---
+++ Class works/24.07.2023/index1.js
@@ -0,0 +1,28 @@
+
+var sum, diff, mul, the_div // ...
+var a = 4
+var b = 5
+
+sum = a + b; // 4 + 5 = 9
+diff = a - b;
+mul = a * b;
+the_div = a / b;
+var power2_a = a * a
+var power2_b = b * b
+var avg_a_b = (a + b) / 2
+var sqrt_a = Math.sqrt( a )
+var sqrt_b = Math.sqrt( b )
+
+document.write("<h3>Hello JS! </h3>");
+document.write("a + b = ", sum)
+document.write("<br /> a - b = ", diff)
+document.write("<br /> a * b = ", mul)
+document.write("<br /> a / b = ", the_div)
+document.write("<br /> a * a = ", power2_a)
+document.write("<br /> b * b = ", power2_b)
+document.write("<br /> avg (a, b) = ", avg_a_b)
+document.write("<br /> a * a = ", power2_b)
+document.write("<br /> a sqrt = ", sqrt_a)
+document.write("<br /> b sqrt = ", sqrt_b)
+
+
