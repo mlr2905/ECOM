55b4f756fe77a7944bfd963fa681769522efd430
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2023-09-13 22:41:38 +0300

    up

---
+++ Homework/04.09.23/car.js
@@ -0,0 +1,12 @@
+class car{
+    constructor (_brand,_model,_price,_license,_year,_color,_maintenance) {
+        this.brand = _brand
+        this.model = _model
+        this.price = _price
+        this.license = _license
+        this.year = _year
+        this.color = _color
+        this.maintenance = _maintenance
+
+    }
+}
