16e3d3027d137a359de12d979522f3ff9a835ea2
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2023-09-14 11:55:58 +0300

    ופ

---
+++ Homework/04.09.23/phone.js
@@ -0,0 +1,11 @@
+class phone{
+    constructor (_brand,_model,_color,_storageCapacity,_batteryLife,_poweredOn) {
+        this.brand = _brand
+        this.model = _model
+        this.color = _color
+        this.storageCapacity = _storageCapacity
+        this.batteryLife = _batteryLife
+        this.poweredOn = _poweredOn
+
+    }
+}
